b6b05c6aaaedede1a012964e90518495907fd0eb
Gustavo Souza <125296147+guslwl@users.noreply.github.com>
2025-11-23 23:11:15 +0000

    finish api/v1/status` endpoint

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,9 +1,37 @@
 import database from "infra/database.js";
 
 async function status(request, response) {
-  const queryResult = await database.query("SELECT 1 + 1;");
-  console.log(queryResult.rows);
-  response.status(200).json({ chave: "em tempo real" });
+  const updatedAt = new Date().toISOString();
+
+  const postgresVersionResult = await database.query("SHOW server_version;");
+  const postgresVersionValue = postgresVersionResult.rows.at(0).server_version;
+
+  const postgresMaxConnectionsResult = await database.query(
+    "SHOW max_connections;",
+  );
+  const postgresMaxConnectionsValue = parseInt(
+    postgresMaxConnectionsResult.rows[0].max_connections,
+  );
+
+  const databaseName = process.env.POSTGRES_DB;
+  const postgresOpenConnectionsResult = await database.query({
+    text: "SELECT count(*)::int FROM pg_stat_activity WHERE datname = $1",
+    values: [databaseName],
+  });
+
+  const postgresOpenConnectionsValue =
+    postgresOpenConnectionsResult.rows[0].count;
+
+  response.status(200).json({
+    updated_at: updatedAt,
+    dependencies: {
+      database: {
+        version: postgresVersionValue,
+        max_connections: postgresMaxConnectionsValue,
+        open_connections: postgresOpenConnectionsValue,
+      },
+    },
+  });
 }
 
 export default status;
