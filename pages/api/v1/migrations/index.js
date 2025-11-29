0461b8f4fd897359f98bec3bf0cc920fe7f7704a
Gustavo Souza <125296147+guslwl@users.noreply.github.com>
2025-11-29 22:19:21 +0000

    add `/api/v1/migrations` endpoint

---
+++ pages/api/v1/migrations/index.js
@@ -0,0 +1,36 @@
+import migrationRunner from "node-pg-migrate";
+import { join } from "node:path";
+import database from "infra/database";
+
+export default async function migrations(request, response) {
+  const dbClient = await database.getNewClient();
+
+  const defaultMigrationsOptions = {
+    dbClient: dbClient,
+    dir: join("infra", "migrations"),
+    direction: "up",
+    verbose: true,
+    migrationsTable: "pgmigrations",
+    dryRun: true,
+  };
+
+  if (request.method === "GET") {
+    const pendingMigrations = await migrationRunner(defaultMigrationsOptions);
+    await dbClient.end();
+    return response.status(200).json(pendingMigrations);
+  }
+
+  if (request.method === "POST") {
+    const migratedMigrations = await migrationRunner({
+      ...defaultMigrationsOptions,
+      dryRun: false,
+    });
+
+    const responseStatusCode = migratedMigrations.length > 0 ? 201 : 200;
+
+    await dbClient.end();
+    return response.status(responseStatusCode).json(migratedMigrations);
+  }
+
+  return response.status(405).end();
+}
