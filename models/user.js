9b7adef51ee778f3424e4e7ad74c97ab3d230918
Gustavo Souza <125296147+guslwl@users.noreply.github.com>
2026-01-29 17:40:01 +0000

    feat: create `user` model and `/api/v1/status` endpoint

---
+++ models/user.js
@@ -0,0 +1,76 @@
+import database from "infra/database.js";
+import { ValidationError } from "infra/errors.js";
+
+async function create(userInputValues) {
+  await validateUniqueEmail(userInputValues.email);
+  await validateUniqueUsername(userInputValues.username);
+
+  const newUser = await runInsertQuery(userInputValues);
+  return newUser;
+
+  async function validateUniqueUsername(username) {
+    const results = await database.query({
+      text: `
+      SELECT
+        username
+      FROM
+        users
+      WHERE
+        LOWER(username) = LOWER($1)
+    ;`,
+      values: [username],
+    });
+
+    if (results.rowCount > 0) {
+      throw new ValidationError({
+        message: "The username provided is already in use",
+        action: "Use another username",
+      });
+    }
+  }
+
+  async function validateUniqueEmail(email) {
+    const results = await database.query({
+      text: `
+      SELECT
+        email
+      FROM
+        users
+      WHERE
+        LOWER(email) = LOWER($1)
+    ;`,
+      values: [email],
+    });
+
+    if (results.rowCount > 0) {
+      throw new ValidationError({
+        message: "The email provided is already in use",
+        action: "Use another email",
+      });
+    }
+  }
+
+  async function runInsertQuery(userInputValues) {
+    const newUser = await database.query({
+      text: `
+    INSERT INTO
+      users (username, email, password)
+    VALUES
+      ($1, $2, $3)
+    RETURNING
+      *
+    ;`,
+      values: [
+        userInputValues.username,
+        userInputValues.email,
+        userInputValues.password,
+      ],
+    });
+
+    return newUser.rows[0];
+  }
+}
+
+const user = { create };
+
+export default user;
